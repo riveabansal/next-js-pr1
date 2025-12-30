1e7f221eefb6597db9243a03bc147e2fadafffea
Rivea <riveabansal07@gmail.com>
2025-12-30 13:26:04 +0530

    Implement Left and Right Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,22 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex-min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+          <div className="max-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        <RightSidebar />
+      </div>
     </main>
   );
 }
