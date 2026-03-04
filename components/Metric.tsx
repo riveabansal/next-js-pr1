59ecba969baea74d8491bf4bb20c1ccda288b157
Rivea <riveabansal07@gmail.com>
2026-03-04 12:36:46 +0530

    Implement Question Details

--- components/Metric.tsx
+++ components/Metric.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 interface Props {
   imgUrl: string;
@@ -11,9 +11,10 @@ interface Props {
   textStyles: string;
   imgStyles?: string;
   isAuthor?: boolean;
+  titleStyles?: string;
 }
 
-const Metric = ({ imgUrl, alt, value, title, href, textStyles, imgStyles, isAuthor }: Props) => {
+const Metric = ({ imgUrl, alt, value, title, href, textStyles, imgStyles, isAuthor, titleStyles }: Props) => {
   const metricContent = (
     <>
       <Image src={imgUrl} width={16} height={16} alt={alt} className={`rounded-full object-contain ${imgStyles}`} />
@@ -21,7 +22,7 @@ const Metric = ({ imgUrl, alt, value, title, href, textStyles, imgStyles, isAuth
       <p className={`${textStyles} flex items-center gap-1`}>
         {value}
 
-        <span className={`small-regular line-clamp-1 ${isAuthor ? "max-sm:hidden" : ""}`}>{title}</span>
+        {title ? <span className={cn(`small-regular line-clamp-1`, titleStyles)}>{title}</span> : null}
       </p>
     </>
   );
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -8,7 +8,15 @@ import Tag, { ITagDoc } from "@/database/tag.model";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { AskQuestionSchema, EditQuestionSchema, GetQuestionSchema, PaginatedSearchParamsSchema } from "../validations";
+import {
+  AskQuestionSchema,
+  EditQuestionSchema,
+  GetQuestionSchema,
+  IncrementViewsSchema,
+  PaginatedSearchParamsSchema,
+} from "../validations";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
 
 export async function createQuestion(params: CreateQuestionParams): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -171,7 +179,7 @@ export async function getQuestion(params: GetQuestionParams): Promise<ActionResp
   const { questionId } = validationResult.params!;
 
   try {
-    const question = await Question.findById(questionId).populate("tags");
+    const question = await Question.findById(questionId).populate("tags").populate("author", "_id name image");
 
     if (!question) {
       throw new Error("Question not found");
@@ -243,6 +251,40 @@ export async function getQuestions(
     return {
       success: true,
       data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(params: IncrementViewsParams): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    question.views += 1;
+
+    await question.save();
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: { views: question.views },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
