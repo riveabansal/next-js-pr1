816412aeb2b0098c72a9a24dd6aee659ab7e3aef
Rivea <riveabansal07@gmail.com>
2026-03-05 22:13:23 +0530

    Implement Community page

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import React from "react";
 
 import ROUTES from "@/constants/routes";
+import { cn } from "@/lib/utils";
 
 import { Avatar, AvatarFallback } from "./ui/avatar";
-import { cn } from "@/lib/utils";
 
 interface Props {
   id: string;
@@ -25,9 +25,9 @@ const UserAvatar = ({ id, name, imageUrl, className = "h-9 w-9", fallbackClassNa
 
   return (
     <Link href={ROUTES.PROFILE(id)}>
-      <Avatar className={className}>
+      <Avatar className={cn("relative", className)}>
         {imageUrl ? (
-          <Image src={imageUrl} alt={name} className="object-cover" width={36} height={36} quality={100} />
+          <Image src={imageUrl} alt={name} className="object-cover" fill quality={100} />
         ) : (
           <AvatarFallback
             className={cn("primary-gradient font-space-grotesk font-bold tracking-wider text-white", fallbackClassName)}
