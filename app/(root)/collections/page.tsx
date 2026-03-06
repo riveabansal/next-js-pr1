d66026f9be34a2cf9074d8288104a27be872e9f9
Rivea <riveabansal07@gmail.com>
2026-03-06 14:26:46 +0530

    deploy error 6

--- app/(root)/collections/page.tsx
+++ app/(root)/collections/page.tsx
@@ -1,69 +1,61 @@
-import React from "react";
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
+import CommonFilter from "@/components/filter/CommonFilter";
+import { CollectionFilters } from "@/constants/filters";
+import Pagination from "@/components/Pagination";
 
-const Collections = () => {
-  return <div>Collections</div>;
+interface SearchParams {
+  searchParams?: { [key: string]: string | undefined };
+}
+
+const Collections = async ({ searchParams }: SearchParams) => {
+  const { page, pageSize, query, filter } = searchParams ?? {};
+
+  const { success, data, error } = await getSavedQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+    filter: filter || "",
+  });
+
+  const { collection, isNext } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.COLLECTION}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+
+        <CommonFilter filters={CollectionFilters} otherClasses="min-h-[56px] sm:min-w-[170px]" />
+      </div>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={collection}
+        empty={EMPTY_QUESTION}
+        render={(collection) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {collection.map((item) => (
+              <QuestionCard key={item._id} question={item.question} />
+            ))}
+          </div>
+        )}
+      />
+
+      <Pagination page={page} isNext={isNext || false} />
+    </>
+  );
 };
 
 export default Collections;
-
-// import QuestionCard from "@/components/cards/QuestionCard";
-// import DataRenderer from "@/components/DataRenderer";
-// import LocalSearch from "@/components/search/LocalSearch";
-// import ROUTES from "@/constants/routes";
-// import { EMPTY_QUESTION } from "@/constants/states";
-// import { getSavedQuestions } from "@/lib/actions/collection.action";
-// import CommonFilter from "@/components/filter/CommonFilter";
-// import { CollectionFilters } from "@/constants/filters";
-// import Pagination from "@/components/Pagination";
-
-// interface SearchParams {
-//   searchParams?: { [key: string]: string | undefined };
-// }
-
-// const Collections = async ({ searchParams }: SearchParams) => {
-//   const { page, pageSize, query, filter } = searchParams ?? {};
-
-//   const { success, data, error } = await getSavedQuestions({
-//     page: Number(page) || 1,
-//     pageSize: Number(pageSize) || 10,
-//     query: query || "",
-//     filter: filter || "",
-//   });
-
-//   const { collection, isNext } = data || {};
-
-//   return (
-//     <>
-//       <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
-
-//       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-//         <LocalSearch
-//           route={ROUTES.COLLECTION}
-//           imgSrc="/icons/search.svg"
-//           placeholder="Search questions..."
-//           otherClasses="flex-1"
-//         />
-
-//         <CommonFilter filters={CollectionFilters} otherClasses="min-h-[56px] sm:min-w-[170px]" />
-//       </div>
-
-//       <DataRenderer
-//         success={success}
-//         error={error}
-//         data={collection}
-//         empty={EMPTY_QUESTION}
-//         render={(collection) => (
-//           <div className="mt-10 flex w-full flex-col gap-6">
-//             {collection.map((item) => (
-//               <QuestionCard key={item._id} question={item.question} />
-//             ))}
-//           </div>
-//         )}
-//       />
-
-//       <Pagination page={page} isNext={isNext || false} />
-//     </>
-//   );
-// };
-
-// export default Collections;
